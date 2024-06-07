77fbded415a476dd84a71ee577eb88458e0cf4aa
MSergey <masewl@gmail.com>
2024-06-07 14:58:51 +0300

    27.26.24

--- 1_Counter.py
+++ 1_Counter.py
@@ -1,2 +1,8 @@
 from collections import Counter
+
+cars_moscow = ['black', 'black', 'white', 'black', 'black', 'white', 'yellow', 'yellow', 'yellow']
+cars_spb = ['red', 'black', 'black', 'white', 'white', 'yellow', 'yellow', 'red', 'white']
 c = Counter()
+c['red'] += 15
+c['red'] += 15
+print(c)
